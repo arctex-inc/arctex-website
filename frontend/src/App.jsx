6446727199ca17e54a8d3e43d887a148a477d8a6
Chris H <officialchrisho@gmail.com>
2024-05-10 15:44:07 -0700

    Installed react-three/drei. rotating mesh works.
    need to add more in scene and find out how to angle camera downwards correctly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,34 @@
 import { useRef, useState } from 'react'
 import { Canvas } from "@react-three/fiber"
 import './App.css'
+import { PerspectiveCamera } from '@react-three/drei'
 
 function App() {
   const [hovered, hover] = useState(false)
 
   return (
-    <Canvas>
-      <ambientLight />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      
-      {/* <camera angle={15} /> */}
-      <mesh>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'}/>
-      </mesh>
+    // <div className='container'>
+    <>
+      <Canvas className='item1'>
+        <ambientLight />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <PerspectiveCamera angle={30}>
+        </PerspectiveCamera>
 
-    </Canvas>
+        {/* <camera angle={15} /> */}
+        {/* three js uses radians for rotation */}
+        <mesh rotation={[1, 0, 0]}>
+          <boxGeometry args={[2, 2, 2]}/>
+          <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+
+      </Canvas>
+      <section className='item2'>
+        <h1>blah blah blah</h1>
+        <p>blah blah</p>
+      </section>
+    </>
+    // </div>
   )
 }
 
