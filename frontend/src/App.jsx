38bb4dc49fddafb29135c743374a495a1bc58cda
Chris H <officialchrisho@gmail.com>
2024-05-11 10:01:25 -0700

    Added MapControls to move around. Added defaults on perspective camera. Temp ground and building meshes.
    Need to disable the right click orbit option on MapControls to only allow single click drag around.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useRef, useState } from 'react'
 import { Canvas } from "@react-three/fiber"
 import './App.css'
-import { PerspectiveCamera } from '@react-three/drei'
+import { PerspectiveCamera, MapControls } from '@react-three/drei'
 
 function App() {
   const [hovered, hover] = useState(false)
@@ -10,18 +10,23 @@ function App() {
     // <div className='container'>
     <>
       <Canvas className='item1'>
-        <ambientLight />
+        <ambientLight intensity={2} /> 
+        {/* not sure what the default is for ambient light but you need an ambient light */}
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-        <PerspectiveCamera angle={30}>
+        <PerspectiveCamera makeDefault fov={75} position={[0, 5, 5]} rotation={[-1, 0, 0]}>
         </PerspectiveCamera>
 
         {/* <camera angle={15} /> */}
         {/* three js uses radians for rotation */}
-        <mesh rotation={[1, 0, 0]}>
-          <boxGeometry args={[2, 2, 2]}/>
+        <mesh rotation={[0, 1, 0]}>
+          <boxGeometry args={[1, 1, 1]}/>
           <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
         </mesh>
-
+        <mesh rotation={[0, 0, 0]} position={[0,-2,0]}>
+          <boxGeometry args={[10, 1, 10]} />
+          <meshStandardMaterial color={hovered ? 'hotpink' : 'green'} />
+        </mesh>
+        <MapControls />
       </Canvas>
       <section className='item2'>
         <h1>blah blah blah</h1>
