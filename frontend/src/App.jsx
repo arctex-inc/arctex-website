848adf47e968875f5d4bc5d0a1b5cf568d1f44e2
Chris H <officialchrisho@gmail.com>
2024-06-17 20:36:22 -1000

    Updated component structure and files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,15 @@ import { Canvas } from "@react-three/fiber"
 import { Link } from 'react-router-dom';
 import './App.css'
 import { PerspectiveCamera, MapControls } from '@react-three/drei'
+import TextBoxContent from './components/TextboxContent.jsx';
 
 function App() {
   const [hovered, hover] = useState(false)
 
   return (
-    // <div className='container'>
-    <>
-      <Canvas className='item1'>
+    <div className='overflow-hidden h-full'>
+      {/* can't put any non three.js code within the canvas or it'll break */}
+      <Canvas className='item1 overflow-hidden'>
         <ambientLight intensity={2} />
         {/* not sure what the default is for ambient light but you need an ambient light */}
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
@@ -20,42 +21,30 @@ function App() {
         {/* <camera angle={15} /> */}
         {/* three js uses radians for rotation */}
         <mesh rotation={[0, 1, 0]}>
-          <boxGeometry args={[1, 1, 1]}/>
+          <boxGeometry args={[1, 1, 1]} />
           <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
         </mesh>
-        <mesh rotation={[0, 0, 0]} position={[0,-2,0]}>
+        <mesh rotation={[0, 0, 0]} position={[0, -2, 0]}>
           <boxGeometry args={[10, 1, 10]} />
           <meshStandardMaterial color={hovered ? 'hotpink' : 'green'} />
         </mesh>
         <MapControls />
       </Canvas>
-      {/* Side-box*/}
-      <div className="hidden md:block absolute top-0 right-0 h-full w-1/4 bg-white">
-        <h4 className="text-center font-bold">
-          Side Box
-        </h4>
-        <p className="text-2xl">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
-      <div className="md:hidden absolute bottom-0 inset-x-0 bg-white h-1/4">
-        <h4 className="text-center font-bold">
-          Side Box Mobile
-        </h4>
-        <p className="text-2xl">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+      {/* <div className='' >
         <Link to="/shopping">
           <button className="shopping-button">Go to Shopping Page</button>
         </Link>
+      </div> */}
+
+      {/* Side-box on desktop*/}
+      <div className="hidden md:block absolute top-0 right-0 h-full w-1/3 overflow-scroll bg-white m-5 rounded-lg">
+        <TextBoxContent />
+        {/* Bottom textbox on mobile */}
       </div>
-    </>
+      <div className="md:hidden absolute bottom-0 inset-x-0 bg-white h-1/4 overflow-scroll rounded-t-lg">
+        <TextBoxContent />
+      </div>
+    </div>
   )
 }
 
