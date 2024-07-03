1f9fe24c90d4f42d354b5b3f8e440a2c7f757635
Michael Obi <michael.u.obi1@gmail.com>
2024-07-03 14:05:22 -0500

    Updated mobile box

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,9 @@ function App() {
   const [hovered, hover] = useState(false)
 
   return (
-    <div className='overflow-hidden h-full'>
+    <div className="overflow-hidden h-full">
       {/* can't put any non three.js code within the canvas or it'll break */}
-      <Canvas className='item1 overflow-hidden'>
+      <Canvas className="item1 overflow-hidden">
         <ambientLight intensity={2} />
         {/* not sure what the default is for ambient light but you need an ambient light */}
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
@@ -37,11 +37,11 @@ function App() {
       </div> */}
 
       {/* Side-box on desktop*/}
-      <div className="hidden md:block absolute top-0 right-0 h-full w-1/3 overflow-scroll bg-white m-5 rounded-lg">
+      <div className="hidden md:block absolute top-0 right-0 h-full w-1/3 overflow-y-auto bg-white m-5 rounded-lg overscroll-contain">
         <TextBoxContent />
         {/* Bottom textbox on mobile */}
       </div>
-      <div className="md:hidden absolute bottom-0 inset-x-0 bg-white h-1/4 overflow-scroll rounded-t-lg">
+      <div className="md:hidden absolute inset-x-0 bottom-0 bg-white h-1/4 overflow-y-auto rounded-t-lg overscroll-contain">
         <TextBoxContent />
       </div>
     </div>
